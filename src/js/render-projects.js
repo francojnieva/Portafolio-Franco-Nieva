e2e966e0f0a4a65e323ab337bded339cbe08d363
francojnieva <franco97nieva@hotmail.com>
2023-11-29 17:25:26 -0300

    :art: add layout and styles to the menu, about me, technologies and footer sections. install tailwind and iconify.

---
+++ src/js/render-projects.js
@@ -0,0 +1,37 @@
+const projects = [
+    {
+        name: '',
+        description: '',
+        image: '',
+        textAlt: '',
+        titleImg: '',
+        technologies: {
+            iconHTML: '',
+            iconTailwind: '',
+            iconJS: '',
+            iconGit: '',
+        }
+    },
+    {
+        name: '',
+        description: '',
+        image: '',
+        textAlt: '',
+        titleImg: '',
+        technologies: {
+            lenguage: '',
+            icon: ''
+        }
+    },
+    {
+        name: '',
+        description: '',
+        image: '',
+        textAlt: '',
+        titleImg: '',
+        technologies: {
+            lenguage: '',
+            icon: ''
+        }
+    },
+]
