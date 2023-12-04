fea99ed14139a9670c3385ee28bb95d00b836974
francojnieva <franco97nieva@hotmail.com>
2023-12-03 21:50:50 -0300

    :art: add content to projects section, add CV.

--- src/js/render-projects.js
+++ src/js/render-projects.js
@@ -1,21 +1,21 @@
 const projects = [
     {
         name: 'MotoFly',
-        description: '',
+        description: 'E-commerce desarrollado por 5 integrantes durante el curso de Desarrollo Full Stack en Rolling Code School. Además del stack MERN, usamos Git para controlar versiones de código, Trello para gestionar tareas y SCRUM como marco de trabajo ágil.',
         image: './assets/motofly.png',
         textAlt: 'imagen de portada de MotoFly',
         titleImg: 'MotoFly',
         technologies: [
+            './assets/mongo-icon.svg',
+            './assets/express.svg',
             './assets/reactjs.svg',
-            './assets/bootstrap-icon.svg',
-            './assets/git.svg',
             './assets/nodejs-icon.svg'
         ],
         repo: 'https://github.com/ErikaCV/proyecto-final.git'
     },
     {
         name: 'TecnoStore',
-        description: '',
+        description: 'Llevado a cabo por un equipo de 3 integrantes durante el curso en Rolling Code School. El objetivo principal que tuvimos que realizar fue lograr la persistencia de los datos y que el usuario pueda agregar productos al carrito.',
         image: './assets/tecnostore.png',
         textAlt: 'imagen de portada de TecnoStore',
         titleImg: 'TecnoStore',
@@ -28,8 +28,8 @@ const projects = [
         repo: 'https://github.com/francojnieva/RCS-TecnoStore.git'
     },
     {
-        name: 'Portafolio',
-        description: '',
+        name: 'Portafolio Personal',
+        description: 'Descubre mis proyectos personales y grupales, mis conocimientos y las tecnologías que uso como Desarrollador Frontend. Conecta conmigo a través de mis redes o por medio de un formulario de contacto.',
         image: './assets/portafolio.png',
         textAlt: 'imagen de portada de Portafolio',
         titleImg: 'Portafolio',
@@ -43,7 +43,7 @@ const projects = [
     },
     {
         name: 'MovieApp',
-        description: '',
+        description: 'Proyecto ideal para buscar tus películas favoritas y conocer más sobre ellas. Con MovieApp puedes conocer las películas más populares que hay en el mundo, las mejores clasificadas y descrubrir los próximos estrenos.',
         image: './assets/movieapp.png',
         textAlt: 'imagen de portada de MovieApp',
         titleImg: 'MovieApp',
@@ -57,7 +57,7 @@ const projects = [
     },
     {
         name: 'Dashboard Tienda',
-        description: '',
+        description: 'Con este proyecto puse en práctica mis habilidades de maquetación y diseño con Tailwind CSS. Sigo un diseño de autor, el mismo no cuenta con formato móvil y tablet, por lo que implemento un estilo y un layout responsivo.',
         image: './assets/dashboard.png',
         textAlt: 'imagen de portada de Dashboard Tienda de Comida',
         titleImg: 'Dashboard Tienda de Comida',
@@ -71,7 +71,7 @@ const projects = [
     },
     {
         name: 'Clon Netflix',
-        description: '',
+        description: 'Proyecto realizado con el propósito de recrear la página principal y de inicio de sesión de Netflix con Tailwind CSS. Además de poder visualizar el sitio en dispositivos grandes, tambíen puede verse en dispositivos móviles y tablets.',
         image: './assets/clon-netflix.png',
         textAlt: 'imagen de portada del proyecto Clon Netflix',
         titleImg: 'Clon Netflix',
@@ -96,11 +96,11 @@ function renderProjects() {
                 <figure>
                     <img src=${image} title=${titleImg} alt=${textAlt}/>
                 </figure>
-                <div class="card-body p-3 rounded-b-xl bg-[#070c18] text-[#fafafa]">
+                <div class="card-body p-3 rounded-b-xl bg-[#0d1529] text-[#fafafa]">
                     <h2 class="card-title">${name}</h2>
-                    <p class="text-gray-400">${description}Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero non sapiente eaque. Voluptatum modi accusantium eius, repudiandae delectus soluta quas sit perferendis minima maiores voluptatem sint vitae accusamus, laboriosam non!</p>
+                    <p class="text-gray-400">${description}</p>
                     <div class="flex flex-col items-center pt-5 md:flex-row justify-between">
-                        <a href="${repo}" target="_blank" class="link-repo rounded-md bg-gray-700 shadow-md shadow-[#fff]/10"><img class="icon-[mdi--github] text-lg" role="img" aria-hidden="true" /> Repositorio</a>
+                        <a href="${repo}" target="_blank" class="link-repo rounded-md bg-gray-600 shadow-md shadow-[#fff]/10"><img class="icon-[mdi--github] text-lg" role="img" aria-hidden="true" /> Repositorio</a>
                         <div class="flex items-center mt-4 space-x-2 md:mt-0">
                             <img src=${technologies[0]} class="w-6 transition-all hover:-translate-y-1">
                             <img src=${technologies[1]} class="w-6 transition-all hover:-translate-y-1">
